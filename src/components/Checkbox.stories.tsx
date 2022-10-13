43ac79170f0992b6c460b3150e5d4d0806e37279
wagner.brenner13 <wagner.brenner13@gmail.com>
2022-10-13 01:06:10 -0300

    initial commit

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,28 @@
+import {Meta, StoryObj} from "@storybook/react"
+import { Checkbox, CheckboxProps } from "./checkbox";
+import { Text } from "./Text";
+
+
+export default {
+  title: "components/Checkbox",
+  component: Checkbox,
+  args: {
+    children: 'Create account',
+  },
+  argTypes:{},
+  decorators: [
+    (Story) => {
+      return (
+        <div className="flex items-center gap-2"> 
+          {Story()}
+          <Text size="sm">Lembrar-me de mim por 30 dias </Text>
+        </div>
+      )
+    }
+  ]
+
+} as Meta<CheckboxProps> 
+
+export const Default: StoryObj<CheckboxProps> = {};
+
+
